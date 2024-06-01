62f83929e29a0d434651ecb321fdf31849840e44
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-06-01 22:18:05 +1000

    added show/hide loading spinner logic

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const quoteText = document.getElementById("quote");
 const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 // Stores the pulled fetched Responsed into Array.
 let apiQuotes = []; // Using 'let' as the array updates constantly
 
+// Show loading Spinner
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading Spinner
+function hideLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // Function to get a single quote
 const newQuote = () => {
+  showLoadingSpinner();
   // Generates random quotes
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -23,10 +37,12 @@ const newQuote = () => {
   } else {
     quoteText.classList.remove("long-quote");
   }
+  hideLoadingSpinner();
 };
 
 // Get Quotes Function
 async function getQuotes() {
+  showLoadingSpinner();
   // Declare and Store API
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
