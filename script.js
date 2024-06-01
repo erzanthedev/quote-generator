efb80e7e13b574716daf5faf3bb0d71cdc913fe0
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-06-01 20:22:51 +1000

    include new function to filter a single quote

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+// Stores the pulled fetched Responsed into Array.
+let apiQuotes = []; // Using 'let' as the array updates constantly
+
+// Function to get a single quote
+const newQuote = () => {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+};
+
 // Get Quotes Function
 async function getQuotes() {
   // Declare and Store API
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
+    // Uses fetch to pull API
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data[0]);
+    // Converts fetched data into JSON
+    apiQuotes = await response.json();
+    // Function to get a new single Quotes
+    newQuote();
   } catch (error) {
     // Handle Error
     console.log(`Ooops something went wrong: ${error}`);
